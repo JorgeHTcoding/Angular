f23c85ced9f0184e6b85567c6e009b4505576de6
JorgeHTcoding <jht.coding@gmail.com>
2022-06-21 19:57:00 +0200

    subida retos dia2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
 
 const routes: Routes = [
 
  
   {path:"", component: HomeComponent}, 
-  {path:"registro", component: RegistroComponent }
+  {path:"registro", component: RegistroComponent },
+  {path: "perfil", component: PerfilComponent}
 
 ];
 
--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ENVIRONMENT_INITIALIZER, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css']
+
+  
 })
 export class PerfilComponent implements OnInit {
+      
+   
+    public miUsuario:Usuario;
 
-  constructor() { }
+    constructor()
+    {        
+   
+    this.miUsuario = new Usuario(1,"Sleepy","Wombat","imsleepy@wombat.com","../../../assets/img/wombatcito.jpg","wombatcito");
+   
 
+    }
+    enviar(nuevoNombre: HTMLInputElement, nuevoApellido: HTMLInputElement,nuevoCorreo: HTMLInputElement,nuevoURL: HTMLInputElement )
+{
+  console.log(this.miUsuario.nombre);
+  this.miUsuario.nombre = nuevoNombre.value;  
+  console.log(this.miUsuario.nombre);  
+  this.miUsuario.apellidos = nuevoApellido.value; 
+  this.miUsuario.correo = nuevoCorreo.value;   
+  this.miUsuario.correo = nuevoURL.value;   
+   
+}
   ngOnInit(): void {
   }
 
 }
+
